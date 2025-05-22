6bf40042ba9718fc87f1b4dae6e03cd164373e79
Sujan <sujantechx@gmail.com>
2025-05-22 16:23:04 +0530

    v1.01.01

---
+++ travel_Genie/android/app/src/main/kotlin/com/example/travel_genie/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.travel_genie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ travel_Genie/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:travel_genie/Screen/splash/splash_screen.dart';
+
+void main() {
+  runApp( TravelGenie());
+}
+class TravelGenie extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Travel Genie',
+     home: SplashScreen(),
+     debugShowCheckedModeBanner: false,
+   );
+  }
+}
