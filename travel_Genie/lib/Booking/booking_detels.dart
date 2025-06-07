602a6d7bef5dff3d332a1637a92d8a871fef269e
Sujan <sujantechx@gmail.com>
2025-06-07 10:19:30 +0530

    v1.02.2

---
+++ travel_Genie/lib/Booking/booking_detels.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BookingDetels extends StatefulWidget {
+  const BookingDetels({super.key});
+
+  @override
+  State<BookingDetels> createState() => _BookingDetelsState();
+}
+
+class _BookingDetelsState extends State<BookingDetels> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+    );
+  }
+}
